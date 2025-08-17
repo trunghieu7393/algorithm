d618ab15b4c14510535d07720ee05024adf4de73
hieult <trunghieu.7393@gmail.com>
2025-08-17 18:57:36 +0700

    fix palind

--- valid-palindrome.js
+++ valid-palindrome.js
@@ -1,6 +1,4 @@
 const isPalindrome = (s) => {
-  if (s.length <= 1) return false
-
   const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '')
   const reversed = cleaned.split('').reverse().join('')
 
